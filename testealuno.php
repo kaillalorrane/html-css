201f918ca42bda72bc9bcb26bfc19d30457ac002
Kailla <145464372+kaillalorrane@users.noreply.github.com>
2024-04-16 09:57:14 -0300

    Add files via upload

---
+++ testealuno.php
@@ -0,0 +1,12 @@
+
+    <?php
+    var_dump($_GET);
+    echo $_GET['nome'];
+    echo "<br>";
+    echo $_GET['idade'];
+    echo "<br>";
+    echo $_GET['email'];
+    echo "<br>";
+    echo $_GET['sexo'];
+ 
+    ?>
